2357cb93a2010d1a9da50e9be2d0680a4a53a567
ENAML <EthanACline@gmail.com>
2014-06-24 16:17:40 -0400

    set up socket.io on web app, built Saruman backend app (not included in this git repo), connected both up. now able to pass images to Saruman

--- server.js
+++ server.js
@@ -3,7 +3,8 @@
 var express = require('express'),
     path = require('path'),
     fs = require('fs'),
-    mongoose = require('mongoose');
+    mongoose = require('mongoose'),
+    socket = require('./lib/controllers/socket').chat;
 
 /**
  * Main application file
@@ -35,9 +36,11 @@ require('./lib/config/express')(app);
 require('./lib/routes')(app);
 
 // Start server
-app.listen(config.port, config.ip, function () {
+var server = app.listen(config.port, config.ip, function () {
   console.log('Express server listening on %s:%d, in %s mode', config.ip, config.port, app.get('env'));
 });
+
+var socketConnect = new socket(server);
 
 // Expose app
 exports = module.exports = app;
