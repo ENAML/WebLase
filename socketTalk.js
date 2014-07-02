506a1c42b82cbe9d5d71dab125d89c1133c26920
lolusername <to.atilio@gmail.com>
2014-07-02 19:56:42 -0400

    refoctored canvas upload so that each image gets a unique name based on date

--- socketTalk.js
+++ socketTalk.js
@@ -1,13 +1,19 @@
 'use strict';
 
+var fs = require('fs');
+
 module.exports = function(app, sockets){
 
-  sockets.on('connection', function(){
-    console.log('Sarumon Client Connected . gov');
-  });
+  // sockets.on('connection', function(socket){
+  //   console.log('Sarumon Client Connected . gov');
 
-  sockets.on('imageReq', function() {
+	 //  socket.on('imageReq', function() {
+		// 	var fileName = app.get('imageQueue').unshift();
 
-  });
+		// 	fs.readFile(fileName, function(err, buf) {
+		// 		socket.emit('fileSend', {fileName: fileName, buffer: buf});
+		// 	});
+	 //  });
+  // });
 
 };
