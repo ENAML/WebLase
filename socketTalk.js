afa71c686473562459c29c2eb917efe822c174e7
ENAML <EthanACline@gmail.com>
2014-07-03 16:40:10 -0400

    queue with sockets working

--- socketTalk.js
+++ socketTalk.js
@@ -6,13 +6,21 @@ module.exports = function(app, sockets){
 
   sockets.on('connection', function(socket){
     console.log('Sarumon Client Connected . gov');
+    console.log(app.get('imageQueue'));
 
-	  socket.on('imageReq', function() {
-			var fileName = app.get('imageQueue').unshift();
+	  socket.on('imageReq', function(data, callback) {
+			var fileName = app.get('imageQueue').shift();
+      console.log(fileName);
+      // socket.emit('fileSend', fileName);
+      if (!fileName) {
+        callback({fileName: null, buffer: null});
+      } else {
+        fs.readFile(fileName, function(err, buf) {
+          callback({fileName: fileName, buffer: buf});
+        });
+      }
 
-			fs.readFile(fileName, function(err, buf) {
-				socket.emit('fileSend', {fileName: fileName, buffer: buf});
-			});
+
 	  });
   });
 
